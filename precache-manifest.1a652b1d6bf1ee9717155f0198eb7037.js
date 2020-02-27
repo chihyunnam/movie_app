adf2b652f7b2bcbef19c44b4fb777f0649224735
chihyunnam <matthewnam@naver.com>
2020-02-27 17:55:47 +0900

    Updates

---
+++ precache-manifest.1a652b1d6bf1ee9717155f0198eb7037.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01c4ad13d2cc0b748999ec3f34a66501",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "3d29f4f5f45fdcac27f8",
+    "url": "/movie_app/static/css/main.07f8aed4.chunk.css"
+  },
+  {
+    "revision": "96b9738047fe6f62b257",
+    "url": "/movie_app/static/js/2.05d0df41.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_app/static/js/2.05d0df41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d29f4f5f45fdcac27f8",
+    "url": "/movie_app/static/js/main.b269cd43.chunk.js"
+  },
+  {
+    "revision": "c0057b52e72b0f614c37",
+    "url": "/movie_app/static/js/runtime-main.15be7292.js"
+  }
+]);
